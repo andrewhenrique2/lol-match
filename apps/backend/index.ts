231957d4d4674db19ab971b7add907f596fec7ff
andrewhenrique2 <andrewloly06@gmail.com>
2024-10-24 20:14:30 -0300

    Starting the project

---
+++ apps/backend/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+
+const app = express();
+const port = 3001;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello from the backend!');
+});
+
+app.listen(port, () => {
+  console.log(`Backend listening at http://localhost:${port}`);
+});
